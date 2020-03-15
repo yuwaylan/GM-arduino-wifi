09c2c623aeb2afa87f258e646cc599db07d98567
Cheng-Han,YU <0987431104a@gmail.com>
2020-03-15 17:02:44 +0800

    temp

--- rfidtest/rfidsingeltest/rfidsingeltest.ino
+++ rfidtest/rfidsingeltest/rfidsingeltest.ino
@@ -1,6 +1,6 @@
 #include <SPI.h>
 #include <MFRC522.h> // 引用程式庫
-#include <stdio.h>
+//#include <stdio.h>
 
 #define RST_PIN A0 // 讀卡機的重置腳位
 #define SS_PIN 10  // 晶片選擇腳位
@@ -11,7 +11,7 @@
 //**************
 
 MFRC522 mfrc522(SS_PIN, RST_PIN); // 建立MFRC522物件
-
+int count = 0;
 void setup()
 {
   Serial.begin(9600);
@@ -30,14 +30,15 @@ void loop()
     byte *id = mfrc522.uid.uidByte; // 取得卡片的UID
     byte idSize = mfrc522.uid.size; // 取得UID的長度
 
-    Serial.print("PICC type: "); // 顯示卡片類型
-                                 // 根據卡片回應的SAK值（mfrc522.uid.sak）判斷卡片類型
-                                 // MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
-                                 // Serial.println(mfrc522.PICC_GetTypeName(piccType));
+    // Serial.print("PICC type: "); // 顯示卡片類型
+    // 根據卡片回應的SAK值（mfrc522.uid.sak）判斷卡片類型
+    // MFRC522::PICC_Type piccType = mfrc522.PICC_GetType(mfrc522.uid.sak);
+    // Serial.println(mfrc522.PICC_GetTypeName(piccType));
 
     // Serial.print("UID Size: ");       // 顯示卡片的UID長度值
     // Serial.println(idSize);
-
+    Serial.print(count);
+    Serial.print(":");
     for (byte i = 0; i < idSize; i++)
     { // 逐一顯示UID碼
       // Serial.print("id[");
@@ -46,6 +47,7 @@ void loop()
       // Serial.println(id[i], HEX);       // 以16進位顯示UID值
       Serial.print(id[i], HEX);
     }
+    count++;
     Serial.println();
 
     mfrc522.PICC_HaltA(); // 讓卡片進入停止模式
