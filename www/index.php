2c9594f5107274719938dbdfe07188d9d920f225
Cheng-Han,YU <0987431104a@gmail.com>
2020-04-24 11:07:51 +0800

    unity

--- www/index.php
+++ www/index.php
@@ -1,4 +1,9 @@
 <header>
+
+</header>
+
+<body>
+
     <?php
     $db = new mysqli('localhost', 'clientt', 'clientt', 'clientt');
     $clear = 'AAA';
@@ -8,18 +13,18 @@
         mysqli_select_db($db, 'clientt');
         #echo ' DB連線成功 ';
         echo "<h2>目前狀態:</h2>";
-        $sqlcurrent = "SELECT * FROM `clientt` WHERE `sta`=99";
+        $sqlcurrent = "SELECT * FROM `clientt` WHERE `sta`=1";
         $current = $db->query($sqlcurrent);
         $row = mysqli_num_rows($current); //取得資料表資料列數
         $colums = mysqli_num_fields($current); //取得資料表欄位數
 
         $drow = mysqli_fetch_array($current);
         echo "id:";
-        echo $drow['id_counter'];
+        echo "<id value=" . $drow['id_counter'] . ">" . $drow['id_counter'] . "</id>";
         echo " , status:";
-        echo $drow['sta'];
+        echo "<status value=" . $drow['sta'] . ">" . $drow['sta'] . "</status>";
         echo " , U1:";
-        echo $drow['u_pos_1'];
+        echo $drow['u_pos_1']; //u_pos_1 為馬達狀態
         echo " , U2:";
         echo $drow['u_pos_2'];
         echo " , U3:";
@@ -40,13 +45,15 @@
     header('refresh:1');
     ?>
 
-</header>
-
-<body>
-
-
     <h2>要看哪個?</h2>
     <h1><a href="./ud.php?mode=all">console page</a></h1>
     <h1><a href="./bk.php?mode=all">Booking queue</a></h1>
+
+    <?php
+    $myfile = fopen("C:\Users\user\Documents\GitHub\GM-arduino-wifi\Unity part\status.txt", "w") or die("Unable to open file!");
+    $txt = $drow['id_counter'] . "," . $drow['sta'] . "," . $drow['rid_pos'] . "," . $drow['u_pos_1'];
+    fwrite($myfile, $txt);
+    fclose($myfile);
+    ?>
 
 </body>
